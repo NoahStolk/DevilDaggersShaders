be2f5cc1dd0ff4f48d1709e6f9ea1dc309d7b749
Noah Stolk <noah.stolk1997@gmail.com>
2020-11-23 18:45:00 +0100

    Remove more redundant code in warp fragment shaders

--- DevilDaggersShaders/Shaders/Packs/Optimized/dd/warp2_fragment.glsl
+++ DevilDaggersShaders/Shaders/Packs/Optimized/dd/warp2_fragment.glsl
@@ -1,25 +1,10 @@
 // warp2
 #version 330
 uniform mat4 world_matrix;
-uniform vec3 cam_position;
 uniform vec4 uv_scale_offset;
-uniform float light_scale;
 uniform sampler2D tex_diffuse;
-in      vec4 vert_world_position;
 in      vec3 vert_view_dir;
 out     vec4 out_colour0;
-
-vec3 get_lighting(const in vec4 pos, const in vec3 cam_p, float ls);
-
-vec3 get_lighting(const in vec4 pos, const in vec3 cam_p, float ls)
-{
-	vec3 pos2 = pos.xyz / ls;
-	float fade = length(pos2.xyz);
-	fade = 2.0 * clamp((60.0 - fade) / 60.0, 0.0, 1.0);
-	vec3 light = vec3(fade);
-	light += pow(fade, 0.3) * pow(clamp((1.0 - length(pos.xyz - cam_p) / 6.0), 0.0, 0.5), 1.2) * 22.6 * vec3(1.0, 0.5, 0.1);
-	return light * 0.5;
-}
 
 void main()
 {
@@ -29,12 +14,6 @@ void main()
 
 	vec3 diffuse = textureLod(tex_diffuse, hackuv.yx * uv_scale_offset.zw + world_matrix[2].xz, -1.0).rgb * 1.0;
 
-	diffuse = diffuse * diffuse;
- 
-	vec3 result0 = diffuse;
-	vec3 fade = get_lighting(vert_world_position, cam_position, light_scale);
-
-	result0 *= pow(uv_scale_offset.y, 1.2);
-
+	vec3 result0 = diffuse * diffuse * pow(uv_scale_offset.y, 1.2);
 	out_colour0 = vec4(pow(result0, vec3(1.0 / 2.0)), 0.5);
 }
